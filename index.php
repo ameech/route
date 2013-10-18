bfcfeca0a36f655c6b73717929e70db36646bf8a
Adam Meech <ameech@olson.com>
2013-10-18 14:52:04 -0500

    Added basic functionality

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
+function r($path, $method, $closure) {
+    if (getenv('REQUEST_METHOD') === $method && getenv('REQUEST_URI') === $path) {
+        call_user_func($closure, $path);
+    }
+}
 
+r('/route/', 'GET', function($path) {
+    echo 'home page!';
+});
